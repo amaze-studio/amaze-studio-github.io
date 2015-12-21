725c85f15795a5feb7e600110ca82fbf82793a4d
Mike <mkovetsky@gmail.com>
2015-12-21 21:25:18 +0200

    bg slider fix, main buttons inncreased

--- js/zerif.js
+++ js/zerif.js
@@ -4,9 +4,9 @@
 $.vegas('slideshow', {
   delay:7000,
   backgrounds:[
-    { src:'images/backgrounds/bg1.jpg', fade:1000 },
-    { src:'images/backgrounds/bg2.jpg', fade:1000 },
-    { src:'images/backgrounds/bg3.jpg', fade:1000 }
+    { src:'images/bg1.jpg', fade:1000 },
+    { src:'images/bg2.jpg', fade:1000 },
+    { src:'images/bg3.jpg', fade:1000 }
   ]
 });
       
